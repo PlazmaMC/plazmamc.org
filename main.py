5bc6554806db78ac7832fb27f066229fd27d65f0
Alpha <dev@alpha93.kr>
2024-04-05 11:16:48 +0000

    Move to python

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import *
+
+app = FastAPI(
+    docs_url = None,
+    redoc_url = None
+)
+
+@app.get("/", status_code = 308, response_class = responses.RedirectResponse)
+async def root():
+    return "https://github.com/PlazmaMC/PlazmaBukkit"
+
+def main():
+    urls = {
+        "discord": "https://discord.gg/AZwXTA9Pgx"
+    }
+
+    @app.get("/{target:str}", status_code = 308)
+    async def main(target: str):
+        if (target not in urls):
+            return { 404: {"description": "Not found"} }
+
+        return urls[target]
+main()
