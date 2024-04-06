d51e980ca5adf4b5c4da0e1d1704f9919936fac6
Alpha <dev@alpha93.kr>
2024-04-06 14:45:44 +0900

    Update main.py
    Signed-off-by: Alpha <dev@alpha93.kr>

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from fastapi import *
 
 app = FastAPI(
     docs_url = None,
-    redoc_url = None
+    redoc_url = None,
+    default_response_class = responses.RedirectResponsr
 )
 
-@app.get("/", status_code = 308, response_class = responses.RedirectResponse)
+@app.get("/", status_code = 308)
 async def root():
     return "https://github.com/PlazmaMC/PlazmaBukkit"
 
 def main():
     urls = {
+        "downloads": "https://docs.plazmamc.org/plazma/about/downloads",
         "discord": "https://discord.gg/AZwXTA9Pgx"
     }
 
     @app.get("/{target:str}", status_code = 308)
     async def main(target: str):
         if (target not in urls):
-            return { 404: {"description": "Not found"} }
+            return "/"
 
-        return responses.RedirectResponse(urls[target])
+        return urls[target]
 main()
